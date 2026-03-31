32830a76a0618e55f6ec6f36499717655d0c3c9d
Raven <dagohoymarkraven@gmail.com>
2026-03-31 22:47:32 +0800

    fix: email smtp connection issue

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,7 +1,12 @@
 const nodemailer = require('nodemailer');
+const dns = require('dns');
+
+dns.setDefaultResultOrder('ipv4first');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // important: false for 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -9,7 +14,18 @@ const transporter = nodemailer.createTransport({
 });
 
 async function sendEmail({ to, subject, html }) {
-  await transporter.sendMail({ from: process.env.EMAIL_USER, to, subject, html });
+  try {
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      html,
+    });
+    console.log('Email sent to:', to);
+  } catch (error) {
+    console.error('Email error:', error);
+    throw error;
+  }
 }
 
 module.exports = sendEmail;
